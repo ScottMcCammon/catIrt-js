5d1d366b50ce273cfa0a42b782078cd453dcbc35
Scott McCammon <mccammon@uoregon.edu>
2020-10-16 18:11:33 -0700

    update manual-memtest-estWLE to use wleEst_brm_one()
    removes manual wasm memory management

--- test/manual-memtest-estWLE.js
+++ test/manual-memtest-estWLE.js
@@ -1,14 +1,9 @@
 #!/usr/bin/env node
-'use strict';
 const createCatIrtLib = require('../dist/catirtlib');
-let catirtlib = {};
 
-createCatIrtLib().then(function(Module) {
-  catirtlib = Module;
-  const range = [-4.5, 4.5];
-  const theta = [-1.3, 1.3];
-  const uresp = [1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0]; 
-  const itemparams = [
+createCatIrtLib().then(function(catirtlib) {
+  const resp = [1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0]; 
+  const params = [
       [1.55,-1.88,0.12],
       [3.02,-0.38,0.12],
       [1.9,-0.1,0.12],
@@ -51,24 +46,11 @@ createCatIrtLib().then(function(Module) {
       [1.48,0.72,0.12],
   ];
 
-  let i = 1;
   let maxHeap = 0;
-  let u;
+  let i = 1;
+  let u, res;
   while (true) {
-    let mResp = catirtlib.MatrixFromArray([uresp]);
-    let mParams = catirtlib.MatrixFromArray(itemparams);
-    let mRange = catirtlib.MatrixFromArray([range]);
-    let res = catirtlib.wleEst(mResp, mParams, mRange, catirtlib.ModelType.BRM);
-
-    // wasm heap cleanup
-    mResp.delete();
-    mParams.delete();
-    mRange.delete();
-    res.theta.delete();
-    res.info.delete();
-    res.sem.delete();
-    /*
-    */// comment out any of the above deletes to create a memory leak
+    res = catirtlib.wleEst_brm_one(resp, params);
 
     // inspect heap
     if ((i % 10000) === 0) {
